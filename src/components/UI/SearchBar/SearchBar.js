930c73723524c6f992ed501d1bc6eb66a7a4cb55
Adrian Piech <adrianpiech.git@gmail.com>
2022-01-09 17:19:20 +0100

    Add useRef hook on searchbar to focus on input

--- src/components/UI/SearchBar/SearchBar.js
+++ src/components/UI/SearchBar/SearchBar.js
@@ -1,16 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import styles from "./SearchBar.module.css"
 
 const SearchBar = () => {
   const [term, setTerm] = useState('');
-
+  const inputRef = useRef();
   const search = () => {
     console.log('szukaj', term);
   }
 
+  const focusInput = () => {
+    inputRef.current.focus()
+  }
+
+  useEffect(() => {
+    focusInput();
+  }, []);
+
   return (
     <div className="d-flex">
       <input
+        ref={inputRef}
         value={term}
         onKeyDown={e => e.key === 'Enter' && search()}
         onChange={e => setTerm(e.target.value)}
