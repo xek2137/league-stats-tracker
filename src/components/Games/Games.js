e57dd354081acdd2de575372019d5a1e2614116e
Adrian Piech <adrianpiech.git@gmail.com>
2022-01-11 23:31:38 +0100

    Add axios

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import Game from "../Game/Game.js";
 
-class Games extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>Your last games:</h1>
-        <Game />
-        <Game />
-        <Game />
-        <Game />
-        <Game />
-      </div>
-    )
-  }
+const Games = () => {
+  return (
+    <div className="container">
+      <h1>Your last games:</h1>
+      <Game />
+      <Game />
+      <Game />
+      <Game />
+      <Game />
+    </div>
+  )
 }
 
 export default Games;
